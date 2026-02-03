bed51da91e0948dd5cb05131e848ec3015d81795
sergerad <sergerad@miden.team>
2026-02-04 10:25:08 +1300

    Revert "Merge branch 'next' of github.com:0xMiden/miden-node into sergerad-validator-db"
    This reverts commit 411c8e8f196d2fd46d3176fd5676990000f2b97e, reversing
    changes made to 92b16f5f1334c1a14ed79b605763d6e2066892b1.

--- crates/block-producer/src/server/mod.rs
+++ crates/block-producer/src/server/mod.rs
@@ -40,9 +40,6 @@ use crate::store::StoreClient;
 use crate::validator::BlockProducerValidatorClient;
 use crate::{CACHED_MEMPOOL_STATS_UPDATE_INTERVAL, COMPONENT, SERVER_NUM_BATCH_BUILDERS};
 
-#[cfg(test)]
-mod tests;
-
 /// The block producer server.
 ///
 /// Specifies how to connect to the store, batch prover, and block prover components.
