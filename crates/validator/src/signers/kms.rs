327275083adffcf97ac82fae70b34f0d1aa11320
sergerad <sergerad@miden.team>
2026-02-23 11:49:26 +1300

    reword

--- crates/validator/src/signers/kms.rs
+++ crates/validator/src/signers/kms.rs
@@ -69,9 +69,10 @@ impl BlockSigner for KmsSigner {
     type Error = KmsSignerError;
 
     async fn sign(&self, header: &BlockHeader) -> Result<Signature, Self::Error> {
-        // The Validator signs Ethereum-style Keccak-256 digests. AWS KMS does not support SHA-3
-        // hashing for ECDSA keys (ECC_SECG_P256K1 being the corresponding key-spec), so we pre-hash
-        // the message and pass MessageType::Digest. KMS signs the provided 32-byte digest
+        // The Validator produces Ethereum-style ECDSA (secp256k1) signatures over Keccak-256
+        // digests. AWS KMS does not support SHA-3 hashing for ECDSA keys
+        // (ECC_SECG_P256K1 being the corresponding AWS key-spec), so we pre-hash the
+        // message and pass MessageType::Digest. KMS signs the provided 32-byte digest
         // verbatim.
         let msg = header.commitment().to_bytes();
         let digest = Keccak256::hash(&msg);
