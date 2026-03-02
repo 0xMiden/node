a2b595209d42bb5a598d45b05c06ba1990ddccd0
sergerad <sergerad@miden.team>
2026-03-03 10:03:43 +1300

    RM arc clone

--- crates/store/src/server/mod.rs
+++ crates/store/src/server/mod.rs
@@ -115,8 +115,7 @@ impl Store {
 
         // Spawn the proof scheduler as a background task. It will immediately pick up any
         // unproven blocks from previous runs and begin proving them.
-        let proof_scheduler_handle =
-            proof_scheduler::spawn(Arc::clone(&state.db), Arc::clone(&block_prover));
+        let proof_scheduler_handle = proof_scheduler::spawn(state.db.clone(), block_prover);
 
         let rpc_service = store::rpc_server::RpcServer::new(api::StoreApi {
             state: Arc::clone(&state),
