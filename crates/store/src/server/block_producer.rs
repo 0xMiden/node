671630a90b289eb7e4b6171026a438a78256acf6
sergerad <sergerad@miden.team>
2026-03-02 16:11:27 +1300

    Lint

--- crates/store/src/server/block_producer.rs
+++ crates/store/src/server/block_producer.rs
@@ -11,7 +11,7 @@ use miden_node_utils::tracing::OpenTelemetrySpanExt;
 use miden_protocol::Word;
 use miden_protocol::batch::OrderedBatches;
 use miden_protocol::block::{BlockBody, BlockHeader, BlockNumber, SignedBlock};
-use miden_protocol::utils::{Deserializable, Serializable};
+use miden_protocol::utils::Deserializable;
 use tonic::{Request, Response, Status};
 use tracing::Instrument;
 
