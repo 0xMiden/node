3ffa6e97e76a958e6327c834d5e5548eca78b4d9
sergerad <sergerad@miden.team>
2026-03-12 13:12:51 +1300

    anyhow ensure

--- crates/utils/src/fs.rs
+++ crates/utils/src/fs.rs
@@ -6,9 +6,7 @@ use anyhow::Context;
 pub fn ensure_empty_directory(directory: &Path) -> anyhow::Result<()> {
     if fs_err::exists(directory)? {
         let is_empty = fs_err::read_dir(directory)?.next().is_none();
-        if !is_empty {
-            anyhow::bail!(format!("{} exists but it is not empty.", directory.display()));
-        }
+        anyhow::ensure!(is_empty, "{} exists but is not empty", directory.display());
     } else {
         fs_err::create_dir(directory).with_context(|| {
             format!(
