18159eb5078357b440b59050ae96ec0829cf29e4
Bobbin Threadbare <bobbinth@protonmail.com>
2026-03-24 19:48:36 -0700

    chore: fix build by reverting last commit

--- bin/remote-prover/src/server/prover.rs
+++ bin/remote-prover/src/server/prover.rs
@@ -71,7 +71,7 @@ trait ProveRequest: Send + Sync {
         let result = self.prove(input).instrument(prove_span).await;
 
         if let Err(e) = &result {
-            prove_span.set_error(e);
+            tracing::Span::current().set_error(e);
         }
 
         result.map(|output| Self::encode_response(output))
