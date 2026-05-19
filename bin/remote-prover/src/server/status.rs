e3a5c0a585e94feff5e2d2f14896351960670bb2
Ollie202 <ollieweb3@gmail.com>
2026-05-19 13:06:22 +0100

    fix(remote-prover): use generated status handlers

--- bin/remote-prover/src/server/status.rs
+++ bin/remote-prover/src/server/status.rs
@@ -15,24 +15,20 @@ impl StatusService {
 #[tonic::async_trait]
 impl grpc::server::remote_prover_worker_status_api::Status for StatusService {
     type Input = ();
-    type Output = ();
+    type Output = grpc::remote_prover::WorkerStatus;
 
-    async fn full(&self, _request: ()) -> tonic::Result<grpc::remote_prover::WorkerStatus> {
+    async fn handle(&self, _input: Self::Input) -> tonic::Result<Self::Output> {
         Ok(grpc::remote_prover::WorkerStatus {
             version: env!("CARGO_PKG_VERSION").to_string(),
             supported_proof_type: self.kind as i32,
         })
     }
 
-    async fn handle(&self, _input: Self::Input) -> tonic::Result<Self::Output> {
-        unimplemented!()
-    }
-
     fn decode(_request: ()) -> tonic::Result<Self::Input> {
-        unimplemented!()
+        Ok(())
     }
 
-    fn encode(_output: Self::Output) -> tonic::Result<grpc::remote_prover::WorkerStatus> {
-        unimplemented!()
+    fn encode(output: Self::Output) -> tonic::Result<grpc::remote_prover::WorkerStatus> {
+        Ok(output)
     }
 }
