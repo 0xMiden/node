0xMiden/miden-node

refactor(validator): collapse gRPC error mapping into StatusResultExt
Every handler repeated map_err(|err| Status::<code>(format!(...,
err.as_report()))). Add a StatusResultExt extension trait offering
or_internal(context) and or_invalid_argument(context) over any
ErrorReport error, and convert the sign_block and
submit_proven_transaction call sites. Persistence errors now name the
arm that failed (insert vs replace), and the semaphore errors carry
their source chain.

bin/validator/src/server/validator_service/sign_block.rs
@@ -1,14 +1,14 @@
 use std::sync::atomic::Ordering;
 
 use miden_node_proto::generated as grpc;
-use miden_node_tracing::{ErrorReport, Instrument, info_span, miden_instrument};
+use miden_node_tracing::{Instrument, info_span, miden_instrument};
 use miden_protocol::Word;
 use miden_protocol::block::{BlockNumber, ProposedBlock};
 use miden_protocol::crypto::dsa::ecdsa_k256_keccak::{PublicKey, Signature};
 use miden_protocol::transaction::{TransactionHeader, TransactionId};
 use miden_tx::utils::serde::{Deserializable, Serializable};
 
-use super::ValidatorService;
+use super::{StatusResultExt, ValidatorService};
 use crate::COMPONENT;
 
 #[tonic::async_trait]
@@ -21,11 +21,8 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
         err,
     )]
     fn decode(request: grpc::blockchain::ProposedBlock) -> tonic::Result<Self::Input> {
-        ProposedBlock::read_from_bytes(&request.proposed_block).map_err(|err| {
-            tonic::Status::invalid_argument(
-                err.as_report_context("Failed to deserialize proposed block"),
-            )
-        })
+        ProposedBlock::read_from_bytes(&request.proposed_block)
+            .or_invalid_argument("Failed to deserialize proposed block")
     }
 
     #[miden_instrument(
@@ -59,18 +56,14 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
             .acquire()
             .instrument(info_span!("acquire_permit"))
             .await
-            .map_err(|err| {
-                tonic::Status::internal(format!("sign_block semaphore closed: {err}"))
-            })?;
+            .or_internal("sign_block semaphore closed")?;
 
         // Load the current chain tip from the database.
         let chain_tip = self
             .db
             .load_chain_tip()
             .await
-            .map_err(|err| {
-                tonic::Status::internal(format!("Failed to load chain tip: {}", err.as_report()))
-            })?
+            .or_internal("Failed to load chain tip")?
             .ok_or_else(|| tonic::Status::internal("Chain tip not found in database"))?;
 
         // Capture the block's transactions in block order before the proposed block is consumed, so
@@ -83,13 +76,10 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
         let chain_tip_num = chain_tip.block_num();
 
         // Validate the block against the current chain tip.
-        let (signature, header) =
-            self.validate_block(proposed_block, chain_tip).await.map_err(|err| {
-                tonic::Status::invalid_argument(format!(
-                    "Failed to validate block: {}",
-                    err.as_report()
-                ))
-            })?;
+        let (signature, header) = self
+            .validate_block(proposed_block, chain_tip)
+            .await
+            .or_invalid_argument("Failed to validate block")?;
 
         // Capture the commitment that was signed before `header` is moved into the persistence
         // closure, so it can be returned to the block producer for cross-checking.
@@ -100,14 +90,17 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
         // replaced block — atomically with persisting its successor, so a crash cannot leave the
         // replaced block's links behind.
         let new_block_num = header.block_num().as_u32();
-        let persisted = if header.block_num() == chain_tip_num {
-            self.db.replace_signed_block(header, block_transactions).await
+        if header.block_num() == chain_tip_num {
+            self.db
+                .replace_signed_block(header, block_transactions)
+                .await
+                .or_internal("Failed to replace signed block")?;
         } else {
-            self.db.insert_signed_block(header, block_transactions).await
-        };
-        persisted.map_err(|err| {
-            tonic::Status::internal(format!("Failed to persist block header: {}", err.as_report()))
-        })?;
+            self.db
+                .insert_signed_block(header, block_transactions)
+                .await
+                .or_internal("Failed to insert signed block")?;
+        }
 
         // Update the in-memory counters after successful persistence. The block has already been
         // backed up to the block store by `validate_block`, so it is available to subscribers by

0xMiden/node

bin/validator/src/server/validator_service/sign_block.rs
@@ -2,13 +2,13 @@ use std::sync::atomic::Ordering;
 
 use miden_node_proto::{BlockProofRequest, generated as grpc};
 use miden_node_tracing::spawn::spawn_blocking_in_current_span;
-use miden_node_tracing::{ErrorReport, Instrument, info_span, miden_instrument};
+use miden_node_tracing::{Instrument, info_span, miden_instrument};
 use miden_protocol::Word;
 use miden_protocol::block::{BlockNumber, ProposedBlock};
 use miden_protocol::crypto::dsa::ecdsa_k256_keccak::{PublicKey, Signature};
 use miden_protocol::transaction::{TransactionHeader, TransactionId};
 
-use super::ValidatorService;
+use super::{StatusResultExt, ValidatorService};
 use crate::COMPONENT;
 
 #[tonic::async_trait]
@@ -55,9 +55,7 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
             .acquire()
             .instrument(info_span!("acquire_permit"))
             .await
-            .map_err(|err| {
-                tonic::Status::internal(format!("sign_block semaphore closed: {err}"))
-            })?;
+            .or_internal("sign_block semaphore closed")?;
 
         let proposed_block = spawn_blocking_in_current_span(move || {
             let request = BlockProofRequest::try_from(request).map_err(tonic::Status::from)?;
@@ -71,21 +69,17 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
                     .with_next_validator_config(request.block_header.validator_config().clone())
                     .with_next_protocol_config(request.block_header.next_protocol_config().cloned())
             })
-            .map_err(|error| tonic::Status::invalid_argument(error.to_string()))
+            .or_invalid_argument("Failed to build proposed block")
         })
         .await
-        .map_err(|error| {
-            tonic::Status::internal(format!("block decoding task failed: {error}"))
-        })??;
+        .or_internal("Block decoding task failed")??;
 
         // Load the current chain tip from the database.
         let chain_tip = self
             .db
             .load_chain_tip()
             .await
-            .map_err(|err| {
-                tonic::Status::internal(format!("Failed to load chain tip: {}", err.as_report()))
-            })?
+            .or_internal("Failed to load chain tip")?
             .ok_or_else(|| tonic::Status::internal("Chain tip not found in database"))?;
 
         // Capture the block's transactions in block order before the proposed block is consumed, so
@@ -98,13 +92,10 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
         let chain_tip_num = chain_tip.block_num();
 
         // Validate the block against the current chain tip.
-        let (signature, header) =
-            self.validate_block(proposed_block, chain_tip).await.map_err(|err| {
-                tonic::Status::invalid_argument(format!(
-                    "Failed to validate block: {}",
-                    err.as_report()
-                ))
-            })?;
+        let (signature, header) = self
+            .validate_block(proposed_block, chain_tip)
+            .await
+            .or_invalid_argument("Failed to validate block")?;
 
         // Capture the commitment that was signed before `header` is moved into the persistence
         // closure, so it can be returned to the block producer for cross-checking.
@@ -115,14 +106,17 @@ impl grpc::server::validator_api::SignBlock for ValidatorService {
         // replaced block — atomically with persisting its successor, so a crash cannot leave the
         // replaced block's links behind.
         let new_block_num = header.block_num().as_u32();
-        let persisted = if header.block_num() == chain_tip_num {
-            self.db.replace_signed_block(header, block_transactions).await
+        if header.block_num() == chain_tip_num {
+            self.db
+                .replace_signed_block(header, block_transactions)
+                .await
+                .or_internal("Failed to replace signed block")?;
         } else {
-            self.db.insert_signed_block(header, block_transactions).await
-        };
-        persisted.map_err(|err| {
-            tonic::Status::internal(format!("Failed to persist block header: {}", err.as_report()))
-        })?;
+            self.db
+                .insert_signed_block(header, block_transactions)
+                .await
+                .or_internal("Failed to insert signed block")?;
+        }
 
         // Update the in-memory counters after successful persistence. The block has already been
         // backed up to the block store by `validate_block`, so it is available to subscribers by