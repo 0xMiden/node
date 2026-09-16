0xPolygonMiden/miden-node

feat(funding-service): add the RequestFunds endpoint

bin/funding-service/src/account.rs
@@ -3,8 +3,10 @@
 use std::path::Path;
 
 use anyhow::{Context, Result};
+use miden_protocol::Word;
 use miden_protocol::account::auth::AuthSecretKey;
 use miden_protocol::account::{AccountFile, AccountId, AccountType};
+use miden_protocol::crypto::dsa::falcon512_poseidon2::SecretKey;
 
 // FUNDER KEY
 // ================================================================================================
@@ -13,6 +15,8 @@ use miden_protocol::account::{AccountFile, AccountId, AccountType};
 #[derive(Clone, Debug)]
 pub struct FunderKey {
     account_id: AccountId,
+    secret_key: SecretKey,
+    code_commitment: Word,
 }
 
 impl FunderKey {
@@ -21,10 +25,13 @@ impl FunderKey {
         let account_file = AccountFile::read(path)
             .with_context(|| format!("failed to read the account file at {}", path.display()))?;
 
-        account_file
+        let secret_key = account_file
             .auth_secret_keys
             .iter()
-            .find(|key| matches!(key, AuthSecretKey::Falcon512Poseidon2(_)))
+            .find_map(|key| match key {
+                AuthSecretKey::Falcon512Poseidon2(secret_key) => Some(secret_key.clone()),
+                _ => None,
+            })
             .with_context(|| {
                 format!(
                     "the account file at {} holds no Falcon512Poseidon2 secret key",
@@ -40,11 +47,27 @@ impl FunderKey {
             account.id(),
         );
 
-        Ok(Self { account_id: account.id() })
+        Ok(Self {
+            account_id: account.id(),
+            secret_key,
+            code_commitment: account.code().commitment(),
+        })
     }
 
     pub fn account_id(&self) -> AccountId {
         self.account_id
+    }
+
+    pub fn secret_key(&self) -> &SecretKey {
+        &self.secret_key
+    }
+
+    /// The commitment to the account code in the account file.
+    ///
+    /// Compared against the code of the account on chain, so an account file from another network
+    /// fails at startup instead of as an opaque execution error.
+    pub fn code_commitment(&self) -> Word {
+        self.code_commitment
     }
 }
 
@@ -53,7 +76,6 @@ mod tests {
     use miden_protocol::ONE;
     use miden_protocol::account::auth::AuthScheme;
     use miden_protocol::account::{Account, AccountType};
-    use miden_protocol::crypto::dsa::falcon512_poseidon2::SecretKey;
     use miden_standards::account::auth::Approver;
     use miden_standards::account::wallets::create_basic_wallet;
     use rand::{RngExt, SeedableRng};
@@ -93,12 +115,14 @@ mod tests {
         let path = write_account_file(
             dir.path(),
             &account,
-            vec![AuthSecretKey::Falcon512Poseidon2(secret_key)],
+            vec![AuthSecretKey::Falcon512Poseidon2(secret_key.clone())],
         );
 
         let funder = FunderKey::load(&path).expect("a public wallet with a key should load");
 
         assert_eq!(funder.account_id(), account.id());
+        assert_eq!(funder.code_commitment(), account.code().commitment());
+        assert_eq!(funder.secret_key().public_key(), secret_key.public_key());
     }
 
     /// The service reads the funder's vault from the node, which is only possible for a public

bin/funding-service/src/lib.rs
@@ -1,9 +1,11 @@
 //! The service owns one wallet account which holds the chain's native asset, and sends that asset
-//! to any account which asks for it.
+//! to any account which asks for it. Each request produces a private pay-to-ID note, which the
+//! service returns once the note is committed in a block.
 
 // Required by code generated by the upstream `#[instrument]` macro.
 extern crate miden_node_tracing as tracing;
 
+use std::num::{NonZeroU16, NonZeroUsize};
 use std::path::PathBuf;
 use std::time::Duration;
 
@@ -14,18 +16,30 @@ use miden_node_utils::shutdown::CancellationToken;
 use miden_node_utils::tasks::Tasks;
 use miden_protocol::account::AccountId;
 use miden_protocol::asset::FungibleAsset;
+use miden_protocol::crypto::dsa::ecdsa_k256_keccak::PublicKey as ValidatorPublicKey;
 use tokio::net::TcpListener;
+use tokio::sync::mpsc;
 use url::Url;
 
 use crate::account::FunderKey;
 use crate::node::RpcNodeClient;
+use crate::prover::Prover;
 use crate::server::FundingRpcServer;
 use crate::status::{StatusRefresher, StatusSnapshot};
+use crate::worker::{Funder, FunderSetup, WorkerConfig};
 
 mod account;
+mod data_store;
+mod error;
+mod inclusion;
 mod node;
+mod prover;
 mod server;
 mod status;
+#[cfg(test)]
+mod test_utils;
+mod tx;
+mod worker;
 
 // CONSTANTS
 // =================================================================================================
@@ -38,14 +52,38 @@ pub const LOG_TARGET: &str = "user::miden-funding-service";
 /// Default largest amount one request may ask for, in base units of the native asset.
 pub const DEFAULT_MAX_AMOUNT: u64 = 1_000_000_000;
 
+/// Default largest number of notes one funding transaction creates.
+pub const DEFAULT_MAX_NOTES_PER_TX: NonZeroUsize =
+    NonZeroUsize::new(16).expect("literal is non-zero");
+
+/// The node limits how many note IDs one lookup may hold, and the service looks up a whole
+/// transaction's notes at once.
+const _: () = assert!(DEFAULT_MAX_NOTES_PER_TX.get() <= MAX_NOTES_PER_TX.get());
+
+/// Hard bound on `--max-notes-per-tx`.
+pub const MAX_NOTES_PER_TX: NonZeroUsize = NonZeroUsize::new(100).expect("literal is non-zero");
+
+/// Default number of blocks after its reference block at which a funding transaction expires.
+pub const DEFAULT_TX_EXPIRATION_DELTA: NonZeroU16 =
+    NonZeroU16::new(50).expect("literal is non-zero");
+
+/// Default interval at which the service asks the node whether its notes are committed.
+pub const DEFAULT_POLL_INTERVAL: Duration = Duration::from_secs(1);
+
 /// Default timeout of a request to the node's RPC API.
 pub const DEFAULT_RPC_TIMEOUT: Duration = Duration::from_secs(10);
+
+/// Default timeout of a request to the remote prover.
+pub const DEFAULT_TX_PROVER_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// Default timeout of a gRPC request served by this service.
 pub const DEFAULT_GRPC_TIMEOUT: Duration = Duration::from_secs(300);
 
-/// How often the service reads the funding account from the node.
+/// How often the service reads the funding account while no request arrives.
 const STATUS_REFRESH_INTERVAL: Duration = Duration::from_secs(30);
+
+/// How many requests may wait in the queue, per note of one transaction.
+const QUEUE_CAPACITY_PER_TX: usize = 4;
 
 // CONFIGURATION
 // =================================================================================================
@@ -54,21 +92,44 @@ const STATUS_REFRESH_INTERVAL: Duration = Duration::from_secs(30);
 pub struct FundingServiceConfig {
     rpc_url: Url,
     account_file: PathBuf,
+    validator_signing_public_keys: Vec<ValidatorPublicKey>,
+    tx_prover_url: Option<Url>,
     grpc_timeout: Duration,
     rpc_timeout: Duration,
+    tx_prover_timeout: Duration,
     max_amount: u64,
+    max_notes_per_tx: NonZeroUsize,
+    tx_expiration_delta: NonZeroU16,
+    poll_interval: Duration,
 }
 
 impl FundingServiceConfig {
     /// Creates a configuration with default timeouts and limits.
-    pub fn new(rpc_url: Url, account_file: PathBuf) -> Self {
+    pub fn new(
+        rpc_url: Url,
+        account_file: PathBuf,
+        validator_signing_public_keys: Vec<ValidatorPublicKey>,
+    ) -> Self {
         Self {
             rpc_url,
             account_file,
+            validator_signing_public_keys,
+            tx_prover_url: None,
             grpc_timeout: DEFAULT_GRPC_TIMEOUT,
             rpc_timeout: DEFAULT_RPC_TIMEOUT,
+            tx_prover_timeout: DEFAULT_TX_PROVER_TIMEOUT,
             max_amount: DEFAULT_MAX_AMOUNT,
+            max_notes_per_tx: DEFAULT_MAX_NOTES_PER_TX,
+            tx_expiration_delta: DEFAULT_TX_EXPIRATION_DELTA,
+            poll_interval: DEFAULT_POLL_INTERVAL,
         }
+    }
+
+    /// Sets the remote prover to use. Without one the service proves locally.
+    #[must_use]
+    pub fn with_tx_prover_url(mut self, url: Option<Url>) -> Self {
+        self.tx_prover_url = url;
+        self
     }
 
     #[must_use]
@@ -84,41 +145,92 @@ impl FundingServiceConfig {
     }
 
     #[must_use]
+    pub fn with_tx_prover_timeout(mut self, timeout: Duration) -> Self {
+        self.tx_prover_timeout = timeout;
+        self
+    }
+
+    #[must_use]
     pub fn with_max_amount(mut self, max_amount: u64) -> Self {
         self.max_amount = max_amount;
         self
     }
 
+    #[must_use]
+    pub fn with_max_notes_per_tx(mut self, max_notes_per_tx: NonZeroUsize) -> Self {
+        self.max_notes_per_tx = max_notes_per_tx;
+        self
+    }
+
+    #[must_use]
+    pub fn with_tx_expiration_delta(mut self, delta: NonZeroU16) -> Self {
+        self.tx_expiration_delta = delta;
+        self
+    }
+
+    #[must_use]
+    pub fn with_poll_interval(mut self, interval: Duration) -> Self {
+        self.poll_interval = interval;
+        self
+    }
+
     /// Connects to the node and builds the service.
     pub async fn build(self) -> anyhow::Result<FundingService> {
+        anyhow::ensure!(
+            self.max_notes_per_tx <= MAX_NOTES_PER_TX,
+            "--max-notes-per-tx must not exceed {MAX_NOTES_PER_TX} because the node limits how \
+             many note IDs one lookup may hold",
+        );
+
         let funder_key = FunderKey::load(&self.account_file)
             .context("failed to load the funding account file")?;
 
-        let node = RpcNodeClient::connect(&self.rpc_url, self.rpc_timeout)
-            .await
-            .context("failed to connect to the node RPC API")?;
+        let node = RpcNodeClient::connect(
+            &self.rpc_url,
+            self.rpc_timeout,
+            self.validator_signing_public_keys,
+        )
+        .await
+        .context("failed to connect to the node RPC API")?;
 
         let fee_parameters = node.genesis_header().fee_parameters().clone();
         let fee_faucet_id = fee_parameters.fee_faucet_id();
+        let verification_base_fee = fee_parameters.verification_base_fee();
 
         // A note holds the amount as a fungible asset, so an amount the asset type cannot express
         // must fail at startup instead of on every request.
         FungibleAsset::new(fee_faucet_id, self.max_amount)
             .context("--max-amount is not a valid amount of the native asset")?;
 
+        let prover = match self.tx_prover_url {
+            Some(url) => Prover::remote(url, self.tx_prover_timeout)
+                .context("failed to build the remote prover client")?,
+            None => Prover::local(),
+        };
+
         info!(
             target: LOG_TARGET,
             "Funding service initialized",
             account.id = funder_key.account_id(),
             asset.faucet_id = fee_faucet_id,
-            fee.verification_base_fee = fee_parameters.verification_base_fee(),
-            funding_service.max_amount = self.max_amount
+            fee.verification_base_fee = verification_base_fee,
+            funding_service.max_amount = self.max_amount,
+            funding_service.max_notes_per_tx = self.max_notes_per_tx.get(),
+            funding_service.tx_expiration_delta = self.tx_expiration_delta.get(),
+            funding_service.remote_prover = matches!(prover, Prover::Remote(_))
         );
 
         Ok(FundingService {
             node,
+            prover,
             funder_key,
             fee_faucet_id,
+            verification_base_fee,
+            worker_config: WorkerConfig {
+                max_notes_per_tx: self.max_notes_per_tx,
+                expiration_delta: self.tx_expiration_delta,
+                poll_interval: self.poll_interval,
+            },
             max_amount: self.max_amount,
             grpc_timeout: self.grpc_timeout,
         })
@@ -131,14 +243,17 @@ impl FundingServiceConfig {
 /// The funding service, ready to run.
 pub struct FundingService {
     node: RpcNodeClient,
+    prover: Prover,
     funder_key: FunderKey,
     fee_faucet_id: AccountId,
+    verification_base_fee: u32,
+    worker_config: WorkerConfig,
     max_amount: u64,
     grpc_timeout: Duration,
 }
 
 impl FundingService {
-    /// Runs the gRPC server and the status refresher until one of them stops.
+    /// Runs the gRPC server, the status refresher and the funding worker until one of them stops.
     pub async fn run(
         self,
         listener: TcpListener,
@@ -153,10 +268,12 @@ impl FundingService {
             .await;
 
         let status = StatusSnapshot::new(self.funder_key.account_id(), self.max_amount);
+        let (requests, request_receiver) =
+            mpsc::channel(QUEUE_CAPACITY_PER_TX * self.worker_config.max_notes_per_tx.get());
 
         let mut tasks = Tasks::new();
 
-        let server = FundingRpcServer::new(status.clone(), self.grpc_timeout);
+        let server = FundingRpcServer::new(requests, status.clone(), self.grpc_timeout);
         let server_shutdown = shutdown.clone();
         tasks.spawn("grpc-server", async move {
             server
@@ -166,10 +283,10 @@ impl FundingService {
         });
 
         let refresher = StatusRefresher::new(
-            self.node,
+            self.node.clone(),
             self.funder_key.account_id(),
             self.fee_faucet_id,
-            status,
+            status.clone(),
             STATUS_REFRESH_INTERVAL,
         );
         let refresher_shutdown = shutdown.clone();
@@ -178,6 +295,25 @@ impl FundingService {
                 .run(refresher_shutdown)
                 .await
                 .context("the funding service status refresher failed")
+        });
+
+        let funder = Funder::new(
+            self.node,
+            self.prover,
+            FunderSetup {
+                key: self.funder_key,
+                fee_faucet_id: self.fee_faucet_id,
+                verification_base_fee: self.verification_base_fee,
+                config: self.worker_config,
+                status,
+            },
+        );
+        let worker_shutdown = shutdown.clone();
+        tasks.spawn("funder", async move {
+            funder
+                .run(request_receiver, worker_shutdown)
+                .await
+                .context("the funding worker failed")
         });
 
         tasks

0xMiden/node

bin/funding-service/src/account.rs
@@ -3,6 +3,8 @@
 use std::path::Path;
 
 use anyhow::{Context, Result};
+use miden_protocol::Word;
+use miden_protocol::account::auth::AuthSecretKey;
 use miden_protocol::account::{AccountFile, AccountId, AccountType};
 
 // FUNDER KEY
@@ -12,6 +14,8 @@ use miden_protocol::account::{AccountFile, AccountId, AccountType};
 #[derive(Clone, Debug)]
 pub struct FunderKey {
     account_id: AccountId,
+    secret_key: AuthSecretKey,
+    code_commitment: Word,
 }
 
 impl FunderKey {
@@ -19,6 +23,10 @@ impl FunderKey {
     pub fn load(path: &Path) -> Result<Self> {
         let account_file = AccountFile::read(path)
             .with_context(|| format!("failed to read the account file at {}", path.display()))?;
+
+        let secret_key = account_file.auth_secret_keys.first().cloned().with_context(|| {
+            format!("the account file at {} holds no secret key", path.display())
+        })?;
 
         let account = account_file.account;
         anyhow::ensure!(
@@ -28,11 +36,24 @@ impl FunderKey {
             account.id(),
         );
 
-        Ok(Self { account_id: account.id() })
+        Ok(Self {
+            account_id: account.id(),
+            secret_key,
+            code_commitment: account.code().commitment(),
+        })
     }
 
     pub fn account_id(&self) -> AccountId {
         self.account_id
+    }
+
+    pub fn secret_key(&self) -> &AuthSecretKey {
+        &self.secret_key
+    }
+
+    /// The commitment to the account code in the account file.
+    pub fn code_commitment(&self) -> Word {
+        self.code_commitment
     }
 }
 
@@ -81,12 +102,17 @@ mod tests {
         let path = write_account_file(
             dir.path(),
             &account,
-            vec![AuthSecretKey::Falcon512Poseidon2(secret_key)],
+            vec![AuthSecretKey::Falcon512Poseidon2(secret_key.clone())],
         );
 
         let funder = FunderKey::load(&path).expect("a public wallet with a key should load");
 
         assert_eq!(funder.account_id(), account.id());
+        assert_eq!(funder.code_commitment(), account.code().commitment());
+        assert_eq!(
+            funder.secret_key().public_key().to_commitment(),
+            AuthSecretKey::Falcon512Poseidon2(secret_key).public_key().to_commitment()
+        );
     }
 
     /// The service reads the funder's vault from the node, which is only possible for a public

bin/funding-service/src/lib.rs
@@ -1,9 +1,11 @@
 //! The service owns one wallet account which holds the chain's native asset, and sends that asset
-//! to any account which asks for it.
+//! to any account which asks for it. Each request produces a private pay-to-ID note, which the
+//! service returns once the note is committed in a block.
 
 // Required by code generated by the upstream `#[instrument]` macro.
 extern crate miden_node_tracing as tracing;
 
+use std::num::{NonZeroU16, NonZeroUsize};
 use std::path::PathBuf;
 use std::time::Duration;
 
@@ -12,18 +14,31 @@ use miden_node_tracing::info;
 use miden_node_utils::shutdown::CancellationToken;
 use miden_node_utils::tasks::Tasks;
 use miden_protocol::asset::{AssetId, FungibleAsset};
+use miden_protocol::crypto::dsa::ecdsa_k256_keccak::PublicKey as ValidatorPublicKey;
+use miden_protocol::protocol_config::ProtocolConfig;
 use tokio::net::TcpListener;
+use tokio::sync::mpsc;
 use url::Url;
 
 use crate::account::FunderKey;
 use crate::node::RpcNodeClient;
+use crate::prover::Prover;
 use crate::server::FundingServer;
 use crate::status::{StatusRefresher, StatusSnapshot};
+use crate::worker::{Funder, FunderSetup, WorkerConfig};
 
 mod account;
+mod data_store;
+mod error;
+mod inclusion;
 mod node;
+mod prover;
 mod server;
 mod status;
+#[cfg(test)]
+mod test_utils;
+mod tx;
+mod worker;
 
 // CONSTANTS
 // =================================================================================================
@@ -36,14 +51,38 @@ pub const LOG_TARGET: &str = "user::miden-funding-service";
 /// Default largest amount one request may ask for, in base units of the native asset.
 pub const DEFAULT_MAX_AMOUNT: u64 = 1_000_000_000;
 
+/// Default largest number of notes one funding transaction creates.
+pub const DEFAULT_MAX_NOTES_PER_TX: NonZeroUsize =
+    NonZeroUsize::new(16).expect("literal is non-zero");
+
+/// The node limits how many note IDs one lookup may hold, and the service looks up a whole
+/// transaction's notes at once.
+const _: () = assert!(DEFAULT_MAX_NOTES_PER_TX.get() <= MAX_NOTES_PER_TX.get());
+
+/// Hard bound on `--max-notes-per-tx`.
+pub const MAX_NOTES_PER_TX: NonZeroUsize = NonZeroUsize::new(100).expect("literal is non-zero");
+
+/// Default number of blocks after its reference block at which a funding transaction expires.
+pub const DEFAULT_TX_EXPIRATION_DELTA: NonZeroU16 =
+    NonZeroU16::new(50).expect("literal is non-zero");
+
+/// Default interval at which the service asks the node whether its notes are committed.
+pub const DEFAULT_POLL_INTERVAL: Duration = Duration::from_secs(1);
+
 /// Default timeout of a request to the node's RPC API.
 pub const DEFAULT_RPC_TIMEOUT: Duration = Duration::from_secs(10);
+
+/// Default timeout of a request to the remote prover.
+pub const DEFAULT_TX_PROVER_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// Default timeout of an HTTP request served by this service.
 pub const DEFAULT_HTTP_TIMEOUT: Duration = Duration::from_secs(300);
 
-/// How often the service reads the funding account from the node.
+/// How often the service reads the funding account while no request arrives.
 const STATUS_REFRESH_INTERVAL: Duration = Duration::from_secs(30);
+
+/// How many requests may wait in the queue, per note of one transaction.
+const QUEUE_CAPACITY_PER_TX: usize = 4;
 
 // CONFIGURATION
 // =================================================================================================
@@ -52,21 +91,44 @@ const STATUS_REFRESH_INTERVAL: Duration = Duration::from_secs(30);
 pub struct FundingServiceConfig {
     rpc_url: Url,
     account_file: PathBuf,
+    validator_signing_public_keys: Vec<ValidatorPublicKey>,
+    tx_prover_url: Option<Url>,
     http_timeout: Duration,
     rpc_timeout: Duration,
+    tx_prover_timeout: Duration,
     max_amount: u64,
+    max_notes_per_tx: NonZeroUsize,
+    tx_expiration_delta: NonZeroU16,
+    poll_interval: Duration,
 }
 
 impl FundingServiceConfig {
     /// Creates a configuration with default timeouts and limits.
-    pub fn new(rpc_url: Url, account_file: PathBuf) -> Self {
+    pub fn new(
+        rpc_url: Url,
+        account_file: PathBuf,
+        validator_signing_public_keys: Vec<ValidatorPublicKey>,
+    ) -> Self {
         Self {
             rpc_url,
             account_file,
+            validator_signing_public_keys,
+            tx_prover_url: None,
             http_timeout: DEFAULT_HTTP_TIMEOUT,
             rpc_timeout: DEFAULT_RPC_TIMEOUT,
+            tx_prover_timeout: DEFAULT_TX_PROVER_TIMEOUT,
             max_amount: DEFAULT_MAX_AMOUNT,
+            max_notes_per_tx: DEFAULT_MAX_NOTES_PER_TX,
+            tx_expiration_delta: DEFAULT_TX_EXPIRATION_DELTA,
+            poll_interval: DEFAULT_POLL_INTERVAL,
         }
+    }
+
+    /// Sets the remote prover to use. Without one the service proves locally.
+    #[must_use]
+    pub fn with_tx_prover_url(mut self, url: Option<Url>) -> Self {
+        self.tx_prover_url = url;
+        self
     }
 
     #[must_use]
@@ -82,31 +144,75 @@ impl FundingServiceConfig {
     }
 
     #[must_use]
+    pub fn with_tx_prover_timeout(mut self, timeout: Duration) -> Self {
+        self.tx_prover_timeout = timeout;
+        self
+    }
+
+    #[must_use]
     pub fn with_max_amount(mut self, max_amount: u64) -> Self {
         self.max_amount = max_amount;
         self
     }
 
+    #[must_use]
+    pub fn with_max_notes_per_tx(mut self, max_notes_per_tx: NonZeroUsize) -> Self {
+        self.max_notes_per_tx = max_notes_per_tx;
+        self
+    }
+
+    #[must_use]
+    pub fn with_tx_expiration_delta(mut self, delta: NonZeroU16) -> Self {
+        self.tx_expiration_delta = delta;
+        self
+    }
+
+    #[must_use]
+    pub fn with_poll_interval(mut self, interval: Duration) -> Self {
+        self.poll_interval = interval;
+        self
+    }
+
     /// Connects to the node and builds the service.
     pub async fn build(self) -> anyhow::Result<FundingService> {
+        anyhow::ensure!(
+            self.max_notes_per_tx <= MAX_NOTES_PER_TX,
+            "--max-notes-per-tx must not exceed {MAX_NOTES_PER_TX} because the node limits how \
+             many note IDs one lookup may hold",
+        );
+
         let funder_key = FunderKey::load(&self.account_file)
             .context("failed to load the funding account file")?;
 
-        let node = RpcNodeClient::connect(&self.rpc_url, self.rpc_timeout)
-            .await
-            .context("failed to connect to the node RPC API")?;
+        let node = RpcNodeClient::connect(
+            &self.rpc_url,
+            self.rpc_timeout,
+            self.validator_signing_public_keys,
+        )
+        .await
+        .context("failed to connect to the node RPC API")?;
 
         // The fee asset is constant for the chain and is only named by the protocol configuration,
-        let fee_asset_id = node.protocol_config().fee_asset_id();
+        // which the node serves with the genesis block header. The remaining fee parameters are in
+        // every block header, and the status refresher reads them at the block it reports.
+        let protocol_config = node.protocol_config().clone();
+        let fee_asset_id = protocol_config.fee_asset_id();
         let fee_parameters = node
             .fee_parameters(None)
             .await
             .context("failed to read the fee parameters from the node")?;
+        let verification_base_fee = fee_parameters.verification_base_fee();
 
         // A note holds the amount as a fungible asset, so an amount the asset type cannot express
         // must fail at startup instead of on every request.
         FungibleAsset::new(fee_asset_id.faucet_id(), self.max_amount)
             .context("--max-amount is not a valid amount of the native asset")?;
+
+        let prover = match self.tx_prover_url {
+            Some(url) => Prover::remote(url, self.tx_prover_timeout)
+                .context("failed to build the remote prover client")?,
+            None => Prover::local(),
+        };
 
         info!(
             target: LOG_TARGET,
@@ -114,14 +220,25 @@ impl FundingServiceConfig {
             account.id = funder_key.account_id(),
             asset.faucet_id = fee_asset_id.faucet_id(),
             genesis.commitment = node.genesis_commitment(),
-            fee.verification_base_fee = fee_parameters.verification_base_fee(),
-            funding_service.max_amount = self.max_amount
+            fee.verification_base_fee = verification_base_fee,
+            funding_service.max_amount = self.max_amount,
+            funding_service.max_notes_per_tx = self.max_notes_per_tx.get(),
+            funding_service.tx_expiration_delta = self.tx_expiration_delta.get(),
+            funding_service.remote_prover = matches!(prover, Prover::Remote(_))
         );
 
         Ok(FundingService {
             node,
+            prover,
             funder_key,
             fee_asset_id,
+            verification_base_fee,
+            protocol_config,
+            worker_config: WorkerConfig {
+                max_notes_per_tx: self.max_notes_per_tx,
+                expiration_delta: self.tx_expiration_delta,
+                poll_interval: self.poll_interval,
+            },
             max_amount: self.max_amount,
             http_timeout: self.http_timeout,
         })
@@ -134,24 +251,30 @@ impl FundingServiceConfig {
 /// The funding service, ready to run.
 pub struct FundingService {
     node: RpcNodeClient,
+    prover: Prover,
     funder_key: FunderKey,
     fee_asset_id: AssetId,
+    verification_base_fee: u32,
+    protocol_config: ProtocolConfig,
+    worker_config: WorkerConfig,
     max_amount: u64,
     http_timeout: Duration,
 }
 
 impl FundingService {
-    /// Runs the HTTP server and the status refresher until one of them stops.
+    /// Runs the HTTP server, the status refresher and the funding worker until one of them stops.
     pub async fn run(
         self,
         listener: TcpListener,
         shutdown: CancellationToken,
     ) -> anyhow::Result<()> {
         let status = StatusSnapshot::new(self.funder_key.account_id(), self.max_amount);
+        let (requests, request_receiver) =
+            mpsc::channel(QUEUE_CAPACITY_PER_TX * self.worker_config.max_notes_per_tx.get());
 
         let mut tasks = Tasks::new();
 
-        let server = FundingServer::new(status.clone(), self.http_timeout);
+        let server = FundingServer::new(requests, status.clone(), self.http_timeout);
         let server_shutdown = shutdown.clone();
         tasks.spawn("http-server", async move {
             server
@@ -161,10 +284,10 @@ impl FundingService {
         });
 
         let refresher = StatusRefresher::new(
-            self.node,
+            self.node.clone(),
             self.funder_key.account_id(),
             self.fee_asset_id,
-            status,
+            status.clone(),
             STATUS_REFRESH_INTERVAL,
         );
         let refresher_shutdown = shutdown.clone();
@@ -173,6 +296,26 @@ impl FundingService {
                 .run(refresher_shutdown)
                 .await
                 .context("the funding service status refresher failed")
+        });
+
+        let funder = Funder::new(
+            self.node,
+            self.prover,
+            FunderSetup {
+                key: self.funder_key,
+                fee_faucet_id: self.fee_asset_id.faucet_id(),
+                verification_base_fee: self.verification_base_fee,
+                protocol_config: self.protocol_config,
+                config: self.worker_config,
+                status,
+            },
+        );
+        let worker_shutdown = shutdown.clone();
+        tasks.spawn("funder", async move {
+            funder
+                .run(request_receiver, worker_shutdown)
+                .await
+                .context("the funding worker failed")
         });
 
         tasks